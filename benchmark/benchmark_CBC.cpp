f4120dd21142355e122292dc69ec5194cc285293
KienHoSD <channelback546@gmail.com>
2024-06-16 14:40:12 +0700

    add benchmark result

--- benchmark/benchmark_CBC.cpp
+++ benchmark/benchmark_CBC.cpp
@@ -2,7 +2,7 @@
 #include <fstream>
 #include <sys/time.h>
 #include <ctime>
-#include <cstdlib>
+#include <random> // For std::mt19937 and std::uniform_int_distribution
 #include "../src/AES_CBC.h"
 
 const unsigned int MICROSECONDS = 1000000;
@@ -17,11 +17,22 @@ unsigned long getMicroseconds()
 unsigned char *getRandomPlain(unsigned int length)
 {
   unsigned char *plain = new unsigned char[length];
+  std::random_device rd;
+  std::mt19937 gen(rd());
+  std::uniform_int_distribution<> dis(0, 255); // Distribution for random byte values
   for (unsigned int i = 0; i < length; i++)
   {
-    plain[i] = rand() % 256;
+    plain[i] = static_cast<unsigned char>(dis(gen));
   }
   return plain;
+}
+
+int genRandomInt(int min = 0, int max = 0xFFFFFFFF)
+{
+  static std::random_device rd;
+  static std::mt19937 gen(rd());
+  static std::uniform_int_distribution<> dis(min, max);
+  return dis(gen);
 }
 
 int main()
@@ -34,17 +45,21 @@ int main()
   const char key[] = {0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f};
   const char iv[] = "0123456789abcdefdeadbeefdeadbeef";
 
-  std::ofstream results("benchmark_results.csv");
+#ifdef _WIN32
+  std::ofstream results("benchmark_results_win.csv", std::ios::out | std::ios::trunc);
+#else
+  std::ofstream results("benchmark_results_linux.csv");
+#endif
+
   results << "Size(Bytes),Time(Microseconds)\n";
 
   std::cout << "Start speedtest" << std::endl;
-  srand(static_cast<unsigned int>(std::time(nullptr)));
 
   AES_CBC aes(key, iv);
 
   for (unsigned int test = 0; test < NUM_TESTS; ++test)
   {
-    int size = MIN_SIZE + rand() % (MAX_SIZE - MIN_SIZE + 1);
+    int size = genRandomInt(MIN_SIZE, MAX_SIZE);
 
     // add random padding to the plain text
     int padding = 16 - size % 16;
--- benchmark/benchmark_show.py
+++ benchmark/benchmark_show.py
@@ -2,12 +2,18 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 import os
+import platform
 import sys
 
 file_dir = os.path.dirname(__file__)
 sys.path.append(file_dir)
+
 # Load benchmark data from CSV
-df = pd.read_csv('benchmark_results.csv')
+if platform.platform().startswith('Windows'):
+	df = pd.read_csv('benchmark_results_win.csv')
+else:
+	df = pd.read_csv('benchmark_results_linux.csv')
+
 
 # Round input sizes to the nearest kilobyte (assuming sizes are in bytes)
 df['Size(KB)'] = np.round(df['Size(Bytes)'] / 1024)
@@ -17,7 +23,8 @@ grouped = df.groupby('Size(KB)').mean().reset_index()
 
 # Plotting
 plt.figure(figsize=(10, 6))
-plt.plot(grouped['Size(KB)'], grouped['Time(Microseconds)'], marker='o', linestyle='-', color='b', label='AES Encryption Time')
+plt.plot(grouped['Size(KB)'], grouped['Time(Microseconds)'],
+				 marker='o', linestyle='-', color='b', label='AES Encryption Time')
 plt.title('AES Encryption Time vs Input Size')
 plt.xlabel('Input Size (KB)')
 plt.ylabel('Time (Microseconds)')
